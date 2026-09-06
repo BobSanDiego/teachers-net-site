acbea46061c956b883b78b94d87808e05eab3fe5
BobSanDiego <bob.reap@gmail.com>
2026-09-06 15:21:28 -0700

    Update topic composer view contract test

--- tools/community3/test_composer_view.php
+++ tools/community3/test_composer_view.php
@@ -3,7 +3,8 @@ require_once __DIR__ . '/../../wordpress/wp-content/plugins/tnet-community/inclu
 require_once __DIR__ . '/../../wordpress/wp-content/plugins/tnet-community/includes/class-tnet-community-composer-view.php';
 $html='<form method="post" enctype="multipart/form-data"><input id="title" name="title"><textarea id="body" name="body"></textarea></form>';
 $out=TNet_Community_Composer_View::topic($html);
-foreach(['data-composer-view="shared-topic"','name="title"','id="body"','composer-formatting-help'] as $needle) if(strpos($out,$needle)===false) exit(1);
+foreach(['data-composer-view="shared-topic"','name="title"','id="body"'] as $needle) if(strpos($out,$needle)===false) exit(1);
+if(strpos($out,'composer-formatting-help')!==false) exit(1);
 if(strpos($out,'reply-target')!==false) exit(1);
 $reply=TNet_Community_Composer_View::reply('<form id="reply-composer"><input name="submission_id"><input id="reply-target" name="parent_post_id"><textarea id="reply-body" name="body"></textarea><details><summary>Formatting help</summary><p>Use **bold**, *italic*, \`code\`, quotes, lists, or [links](https://example.com).</p></details></form>');
 foreach(['data-composer-view="shared-reply"','name="submission_id"','id="reply-target"','id="reply-body"','composer-formatting-help'] as $needle) if(strpos($reply,$needle)===false) exit(1);
